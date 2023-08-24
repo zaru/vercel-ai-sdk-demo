e4628a0607191bc1f50f34ef41c39fa4fc13e3ad
zaru <zarutofu@gmail.com>
2023-08-25 00:40:24 +0900

    デザインを適用

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,18 +15,40 @@ export default function Home() {
     },
   });
   return (
-    <div>
-      <div>
-        {messages.map((message) => (
-          <div key={message.id}>
-            {message.role === "user" ? "user" : "assistant"}
-            {message.content}
-          </div>
-        ))}
+    <div className="h-screen">
+      <div className="h-full overflow-hidden">
+        <div className="flex h-full flex-col gap-4 overflow-scroll p-8 pb-32">
+          {messages.map((message) => (
+            <div
+              key={message.id}
+              className={`p-4 ${
+                message.role === "user" ? "bg-white" : "bg-gray-50"
+              }`}
+            >
+              {message.role === "user" ? "user" : "assistant"}
+              {message.content}
+            </div>
+          ))}
+        </div>
       </div>
-      <form onSubmit={handleSubmit}>
-        <input value={input} onChange={handleInputChange} className="border" />
-        <button type="submit">投稿</button>
+      <form
+        onSubmit={handleSubmit}
+        className="absolute bottom-0 left-6 right-6 z-10 bg-transparent"
+      >
+        <div className="relative flex items-center justify-center bg-transparent p-4">
+          <input
+            value={input}
+            onChange={handleInputChange}
+            className="w-full rounded-xl border p-4 pr-20 shadow-md"
+            placeholder="メッセージを入力"
+          />
+          <button
+            type="submit"
+            className="absolute right-8 rounded-md bg-sky-600 px-2 py-1.5 text-sm text-white"
+          >
+            投稿
+          </button>
+        </div>
       </form>
     </div>
   );
