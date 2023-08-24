84f47985d1d59abd83291d95d65312370198c03f
zaru <zarutofu@gmail.com>
2023-08-24 15:18:22 +0900

    最低限のチャット動作デモを実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+
+import { useChat } from "ai/react";
+
 export default function Home() {
-  return <div></div>;
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  return (
+    <div>
+      <div>
+        {messages.map((message) => (
+          <div key={message.id}>
+            {message.role === "user" ? "user" : "assistant"}
+            {message.content}
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input value={input} onChange={handleInputChange} className="border" />
+        <button type="submit">投稿</button>
+      </form>
+    </div>
+  );
 }
